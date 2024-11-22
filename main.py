077ca40f19aa25ee98c328e4b6aade6e0849b9b2
kaspersiebrands <kasper.siebrands@gmail.com>
2024-11-22 14:32:09 +0100

    mistake with x and y when drawing line fixed

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def draw_path(screen, path, tile_size):
     if path:  #check if path is found 
         for i in range(len(path) - 1): 
 
-            start_pos = (path[i][0] * tile_size + tile_size // 2, path[i][1] * tile_size + tile_size // 2)
-            end_pos = (path[i + 1][0] * tile_size + tile_size // 2, path[i + 1][1] * tile_size + tile_size // 2)
+            start_pos = (path[i][1] * tile_size + tile_size // 2, path[i][0] * tile_size + tile_size // 2)
+            end_pos = (path[i + 1][1] * tile_size + tile_size // 2, path[i + 1][0] * tile_size + tile_size // 2 )
 
-            pygame.draw.aaline(screen, (255, 0, 0), start_pos, end_pos, 5)  #draws line between start en end point based on values gotten from dijkstra's algoritm
+            pygame.draw.line(screen, (255, 0, 0), start_pos, end_pos, 5)  #draws line between start en end point based on values gotten from dijkstra's algoritm
 
 def main():
     pygame.init()
@@ -32,7 +32,7 @@ def main():
     # Calculate the start and goal points
     start = grid_object.start
     goal = grid_object.end
-
+    
     # Initialize Dijkstra
     dijkstra = Dijkstra(grid_object.grid, TILE_SIZE)
     path = dijkstra.dijkstra_algorithm(start, goal)
